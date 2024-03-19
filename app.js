3c026999a289bf4cf3b03f729b21a08016b76123
rahul dwivedi <rahul.dwivedi@icims.com>
2024-03-19 17:41:17 +0530

    changing port

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/api/properties', require('./routes/properties'));
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
