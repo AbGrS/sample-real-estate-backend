fe6e5ae1cb1afb13d2ed736312e3860a588a9c3d
rahul dwivedi <rahul.dwivedi@icims.com>
2024-03-19 23:28:40 +0530

    updating scheema

---
+++ updateSeed.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Property = require('./models/property');
+require('dotenv').config();
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB Atlas");
+
+
+    Property.updateMany( { enabled : { $exists : false } }, { enabled : false } )
+        .then(() => {
+            console.log("schema updated successfully");
+            mongoose.disconnect();
+        })
+        .catch(err => console.error(err));
+
+}).catch(err => console.error(err));
+
+
